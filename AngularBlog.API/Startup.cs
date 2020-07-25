1e470d9b631ac356efc7934846154ae4cf119a75
evgeniy.zakhartsev <zahartcev777@ya.ru>
2020-07-25 16:55:48 +0300

    added swagger UI Bearer authorization

--- AngularBlog.API/Startup.cs
+++ AngularBlog.API/Startup.cs
@@ -77,6 +77,35 @@ namespace AngularBlog.API
                     Title = "Angular Blog API",
                     Version = Version
                 });
+                
+                #region Bearer Authorization
+
+                sw.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme."
+                });
+                
+                sw.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+                
+                #endregion
 
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
